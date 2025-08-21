6cc6d05c4e01ec8ca61451622a0d580e9e984c8b
derac <dwolson123@gmail.com>
2025-08-21 15:53:49 -0500

    minor changes and cleanup

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 #  TODO
+- Starting with a simple role setup. In the future the role configuration could be extended.
+    - In the SocialCreditConfig, each role could be set up with it's details and use a lambda that takes
+        the user's social credit score and returns True if they are in the group
+        roles need a name, permissions.
+    - To do this, the channels will also need to be configurable in a simular way and use the role
+        objects created in the last step for setting permissions
 - Add group setup
-    - we will have two groups:
+    - we will have two roles:
         - good can access all chats, bad can only access bad chat
         - there is soooo much more you can do here like giving good people the ability to manage nicknames or maybe move bad people around in vc
     - two text chats and two voice chats
         - good text/good voice and bad text/bad voice
         - otherwise doesn't effect the rest of the server
-    - Add setting groups based on score
+    - Add setting roles based on score
         - positive score = you're good,  negative score = you're bad
 - every x time period (adjustable) have social credit bot judge the server
     - the bot will give the user a value between -100 and 100 to add to their social credit score
--- modules/social_credit.py
+++ modules/social_credit.py
@@ -1,5 +1,14 @@
+"""This is a module for creating a SocialCredit bot. It sets up a game where
+it reads messages and assigns social credit scores to users in your chat. These
+scores determine roles that apply priveleges and restrictions.
+
+It creates two roles and two chats, bad and good. After judgement users are sorted
+by whether their social credit score is positive or negative.
+"""
+
 import os, json, time
 from datetime import datetime, timedelta, timezone
+from typing import Literal
 
 import aiohttp, discord
 from discord.ext import commands
@@ -7,14 +16,15 @@ from dotenv import load_dotenv
 
 
 class SocialCredit(commands.Cog):
-    def __init__(self, bot, LOGGER):
+    def __init__(self, bot, logger):
         load_dotenv()
+        #self.config = SocialCreditConfig()
         self.bot = bot
-        self.LOGGER = LOGGER
+        self.logger = logger
+        self.session = aiohttp.ClientSession()
         # these models have what we need and have the largest context
         # https://openrouter.ai/models?order=newest&supported_parameters=structured_outputs&max_price=0
-        self.MODELS = ["qwen/qwen3-235b-a22b:free","google/gemma-3-27b-it:free"]#["qwen/qwen3-235b-a22b:free","meta-llama/llama-3.1-405b-instruct:free","google/gemma-3-27b-it:free", ]
-        self.SESSION = aiohttp.ClientSession()
+        self.MODELS = ["qwen/qwen3-235b-a22b:free", "google/gemma-3-27b-it:free"]
         self.SCAN_PERIOD = timedelta(hours=1)
         self.SYSTEM_PROMPT = "You are a discord bot which seeks to assign a social credit score to all users in \
         a discord server based on the history of their messages from the past day. Good behavior should \
@@ -39,18 +49,16 @@ class SocialCredit(commands.Cog):
             "additionalProperties": False,
         }
 
-
     @commands.command()
     async def judge(self, ctx):
-        AUTHORIZED_USER_ID = int(os.getenv("AUTHORIZED_USER_ID"))
-        if ctx.author.id != AUTHORIZED_USER_ID:
+        if ctx.author.id != int(os.getenv("AUTHORIZED_USER_ID")):
             await ctx.send("You are not authorized to use this command.")
             return
         all_channel_message_log = ""
         all_users_with_messages = set()
 
         for channel in ctx.guild.text_channels:
-            if channel.name in ["bad-people","good-people"]:
+            if channel.name in ["bad-people", "good-people"]:
                 all_channel_message_log += (
                     f"\n# Channel - {channel.name.capitalize()}\n"
                 )
@@ -66,9 +74,13 @@ class SocialCredit(commands.Cog):
                     else:
                         break
         all_users_with_messages = list(all_users_with_messages)
-        #self.LOGGER.debug(all_channel_message_log)
-        self.LOGGER.debug(f"Fetched a message log of length {len(all_channel_message_log)}")
-        self.LOGGER.debug(f"{len(all_users_with_messages)} users: {all_users_with_messages}")
+        # self.logger.debug(all_channel_message_log)
+        self.logger.debug(
+            f"Fetched a message log of length {len(all_channel_message_log)}"
+        )
+        self.logger.debug(
+            f"{len(all_users_with_messages)} users: {all_users_with_messages}"
+        )
         schema_properties = dict()
         for user in all_users_with_messages:
             schema_properties[str(user)] = self.USER_SCHEMA
@@ -97,7 +109,7 @@ class SocialCredit(commands.Cog):
                 "sort": "latency",
                 "data_collection": "deny",
                 "require_parameters": True,
-                "max_price": {"prompt": 0, "completion": 0}
+                "max_price": {"prompt": 0, "completion": 0},
             },
             "response_format": response_format,
             # "temperature": 0.7,
@@ -112,18 +124,18 @@ class SocialCredit(commands.Cog):
         }
 
         while True:
-            async with self.SESSION.post(
+            async with self.session.post(
                 url="https://openrouter.ai/api/v1/chat/completions",
                 headers=headers,
                 data=json.dumps(prompt_config),
             ) as response:
                 if response.status == 200:
                     data = await response.json()
-                    self.LOGGER.debug(data)
+                    self.logger.debug(data)
                     social_credit_scores = json.loads(
                         data["choices"][0]["message"]["content"]
                     )
-                    self.LOGGER.debug(social_credit_scores)
+                    self.logger.debug(social_credit_scores)
                     output = ""
                     for user, data in social_credit_scores.items():
                         social_credit_score = data["social_credit_score"]
@@ -132,47 +144,39 @@ class SocialCredit(commands.Cog):
                     await ctx.send(output)
                     break
                 elif response.status == 429:
-                    self.LOGGER.debug(f"Error {response.status} - Retrying in 1s")
+                    self.logger.debug(f"Error {response.status} - Retrying in 1s")
                     time.sleep(1)
                     continue
                 else:
-                    self.LOGGER.debug(f"Error fetching data: {response.status} - {response}")
+                    self.logger.debug(
+                        f"Error fetching data: {response.status} - {response}"
+                    )
                     break
-            await self.SESSION.close()
+            await self.session.close()
 
+    # TODO: replace data here with config objects that allow the user to set names and permissions etc granularly.
     @commands.command()
     async def setup(self, ctx):
         guild = ctx.guild
-        for role in ["Bad person","Good person"]:
-            if not discord.utils.get(guild.roles, name=role):
+        await self.create_discord_objects(ctx=ctx, names=["Bad person", "Good person"], object_type="role")
+        await self.create_discord_objects(ctx=ctx, names=["Social Credit Simulator"], object_type="category")
+        await self.create_discord_objects(ctx=ctx, names=["bad-people", "good-people"], object_type="text_channel")
+
+    async def create_discord_objects(self, ctx, names: list[str], object_type: Literal["role", "text_channel", "category"]) -> None:
+        object_type_plural = f"{object_type[0:-1]+'ie' if object_type[-1]=='y' else object_type}s"
+        object_type_capitalized = object_type.capitalize()
+        create_object_function = getattr(ctx.guild, f"create_{object_type}")
+        existing_objects = getattr(ctx.guild, object_type_plural)
+        for name in names:
+            if not discord.utils.get(existing_objects, name=name):
                 try:
-                    await guild.create_role(name=role, reason="Role created by bot as it didn't exist.")
-                    await ctx.send(f"Role '{role}' created successfully.")
+                    await create_object_function(
+                        name=name, reason=f"{object_type_capitalized} created by bot as it didn't exist."
+                    )
+                    await ctx.send(f"{object_type_capitalized} '{name}' created successfully.")
                 except discord.Forbidden:
-                    await ctx.send("I don't have permission to create roles.")
+                    await ctx.send("I don't have permission to create {object_type_plural}.")
                 except Exception as e:
-                    await ctx.send(f"An error occurred while creating the role: {e}")
+                    await ctx.send(f"An error occurred while creating the {object_type}: {e}")
             else:
-                await ctx.send(f"The role {role} already exists.")
-        category = "Social Credit Simulator"
-        if not discord.utils.get(guild.categories, name=category):
-            try:
-                await guild.create_category(category)
-                await ctx.send(f"Category '{category}' created successfully.")
-            except discord.Forbidden:
-                await ctx.send("I don't have permission to create categories.")
-            except Exception as e:
-                await ctx.send(f"An error occurred while creating the category: {e}")
-        else:
-            await ctx.send(f"The category {category} already exists.")
-        for channel in ["bad-people","good-people"]:
-            if not discord.utils.get(guild.text_channels, name=channel):
-                try:
-                    await guild.create_text_channel(channel, category=discord.utils.get(guild.categories, name=category))
-                    await ctx.send(f"Channel '{channel}' created successfully.")
-                except discord.Forbidden:
-                    await ctx.send("I don't have permission to create channels.")
-                except Exception as e:
-                    await ctx.send(f"An error occurred while creating the channel: {e}")
-            else:
-                await ctx.send(f"The channel {channel} already exists.")
+                await ctx.send(f"The {object_type} {name} already exists.")
